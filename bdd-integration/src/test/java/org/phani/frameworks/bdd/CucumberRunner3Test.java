95b3926cc726cc6f9391e2832a471cbfda687207
pka23 <phani.kaliginadi@sky.uk>
2022-06-21 23:16:00 +0100

    disabled junit5 cuke test

--- bdd-integration/src/test/java/org/phani/frameworks/bdd/CucumberRunner3Test.java
+++ bdd-integration/src/test/java/org/phani/frameworks/bdd/CucumberRunner3Test.java
@@ -1,10 +1,12 @@
 package org.phani.frameworks.bdd;
 
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+@Disabled
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
